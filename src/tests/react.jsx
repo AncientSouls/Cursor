5698489a06c81039909cf67678802f028697c606
Ivan S Glazunov <ivansglazunov@gmail.com>
2017-08-09 14:52:47 +0000

    fix(tests): React imports.

--- src/tests/react.jsx
+++ src/tests/react.jsx
@@ -3,26 +3,11 @@ require('source-map-support').install();
 import { assert } from 'chai';
 import {
   Cursor,
-  BundlesQueue as BundlesQueueProto,
-  CursorsManager,
-  executeBundle,
-  executers,
 } from '../lib';
-import lodash from 'lodash';
-import mingo from 'mingo';
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import {
-  compose,
-} from 'react-komposer';
-
-class BundlesQueue extends BundlesQueueProto {
-  _handler(id, task, done) {
-    task();
-    done();
-  }
-}
+import { shallow } from 'enzyme';
+import { compose } from 'react-komposer';
 
 export default function() {
   it('react compose container', () => {
