a049980ad8a5e46371aecf8103b41d087ae956b4
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-11 10:57:20 +0300

    es6

--- tests/cursor.js
+++ tests/cursor.js
@@ -1,29 +1,28 @@
 "use strict";
-exports.__esModule = true;
-var chai_1 = require("chai");
-var cursor_1 = require("../lib/cursor");
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+const cursor_1 = require("../lib/cursor");
 function default_1() {
-    describe('Cursor:', function () {
-        it('get()', function () {
-            var cursor = new cursor_1.Cursor();
+    describe('Cursor:', () => {
+        it('get()', () => {
+            const cursor = new cursor_1.Cursor();
             cursor.exec(true, { a: [{ b: { c: 'd' } }] });
             chai_1.assert.equal(cursor.get('a.0.b.c'), 'd');
         });
-        it('apply()', function () {
-            var cursor = new cursor_1.Cursor();
+        it('apply()', () => {
+            const cursor = new cursor_1.Cursor();
             cursor.exec(true, { a: [{ b: { c: 'd' } }] });
             cursor.apply({
                 type: 'set',
                 path: 'a.0.b',
-                value: { d: 'e' }
+                value: { d: 'e' },
             });
             chai_1.assert.equal(cursor.get('a.0.b.d'), 'e');
         });
-        it('changed', function (done) {
-            var cursor = new cursor_1.Cursor();
+        it('changed', (done) => {
+            const cursor = new cursor_1.Cursor();
             cursor.exec(true, { a: [{ b: { c: 'd' } }] });
-            cursor.on('changed', function (_a) {
-                var data = _a.data, oldValue = _a.oldValue, newValue = _a.newValue, bundlePath = _a.bundlePath, bundle = _a.bundle, watch = _a.watch, cursor = _a.cursor;
+            cursor.on('changed', ({ data, oldValue, newValue, bundlePath, bundle, watch, cursor, }) => {
                 chai_1.assert.deepEqual(data, { a: [{ b: { d: 'e' } }] });
                 chai_1.assert.deepEqual(oldValue, { c: 'd' });
                 chai_1.assert.deepEqual(newValue, { d: 'e' });
@@ -32,81 +31,69 @@ function default_1() {
             cursor.apply({
                 type: 'set',
                 path: 'a.0.b',
-                value: { d: 'e' }
+                value: { d: 'e' },
             });
         });
-        it('watch', function (done) {
-            var cursor = new cursor_1.Cursor();
+        it('watch', (done) => {
+            const cursor = new cursor_1.Cursor();
             cursor.exec(true, { a: [{ b: { c: 'd' } }] });
-            cursor.on('changed', function (_a) {
-                var watch = _a.watch;
-                var counter = 0;
-                watch('', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+            cursor.on('changed', ({ watch }) => {
+                let counter = 0;
+                watch('', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isFalse(isClone);
                     chai_1.assert.deepEqual(oldValue, { a: [{ d: { e: 'f' } }] });
                     chai_1.assert.deepEqual(newValue, { a: [{ d: { e: 'f' } }] });
                     counter++;
                 });
-                watch('a', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isFalse(isClone);
                     chai_1.assert.deepEqual(oldValue, [{ d: { e: 'f' } }]);
                     chai_1.assert.deepEqual(newValue, [{ d: { e: 'f' } }]);
                     counter++;
                 });
-                watch('a.0', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.0', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, { b: { c: 'd' } });
                     chai_1.assert.deepEqual(newValue, { d: { e: 'f' } });
                     counter++;
                 });
-                watch(['a', {}, 'b'], function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch(['a', {}, 'b'], ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, { c: 'd' });
                     chai_1.assert.deepEqual(newValue, undefined);
                     counter++;
                 });
-                watch('a.0.b', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.0.b', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, { c: 'd' });
                     chai_1.assert.deepEqual(newValue, undefined);
                     counter++;
                 });
-                watch('a.0.b.c', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.0.b.c', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, 'd');
                     chai_1.assert.deepEqual(newValue, undefined);
                     counter++;
                 });
-                watch('a.0.d', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.0.d', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, undefined);
                     chai_1.assert.deepEqual(newValue, { e: 'f' });
                     counter++;
                 });
-                watch('a.0.d.e', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.0.d.e', ({ isClone, oldValue, newValue }) => {
                     chai_1.assert.isTrue(isClone);
                     chai_1.assert.deepEqual(oldValue, undefined);
                     chai_1.assert.deepEqual(newValue, 'f');
                     counter++;
                 });
-                watch('x.y.z', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('x.y.z', ({ isClone, oldValue, newValue }) => {
                     throw new Error('"x.y.z" path is not changed, wrong watch calling');
                 });
-                watch('a.1', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('a.1', ({ isClone, oldValue, newValue }) => {
                     throw new Error('"a.1" path is not changed, wrong watch calling');
                 });
-                watch('b', function (_a) {
-                    var isClone = _a.isClone, oldValue = _a.oldValue, newValue = _a.newValue;
+                watch('b', ({ isClone, oldValue, newValue }) => {
                     throw new Error('"b" path is not changed, wrong watch calling');
                 });
                 chai_1.assert.equal(counter, 8);
@@ -115,10 +102,10 @@ function default_1() {
             cursor.apply({
                 type: 'set',
                 path: 'a.0',
-                value: { d: { e: 'f' } }
+                value: { d: { e: 'f' } },
             });
         });
     });
 }
-exports["default"] = default_1;
+exports.default = default_1;
 //# sourceMappingURL=cursor.js.map
--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,14 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 require('source-map-support').install();
-var bundle_1 = require("./bundle");
-var cursor_1 = require("./cursor");
-var cursors_manager_1 = require("./cursors-manager");
-var childs_cursors_manager_1 = require("./childs-cursors-manager");
-describe('AncientSouls/Cursor:', function () {
-    bundle_1["default"]();
-    cursor_1["default"]();
-    cursors_manager_1["default"]();
-    childs_cursors_manager_1["default"]();
+const bundle_1 = require("./bundle");
+const cursor_1 = require("./cursor");
+const cursors_manager_1 = require("./cursors-manager");
+const childs_cursors_manager_1 = require("./childs-cursors-manager");
+describe('AncientSouls/Cursor:', () => {
+    bundle_1.default();
+    cursor_1.default();
+    cursors_manager_1.default();
+    childs_cursors_manager_1.default();
 });
 //# sourceMappingURL=index.js.map
