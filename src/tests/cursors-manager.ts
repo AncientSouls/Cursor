ba46e990decfef4cf9577452795481a039ac59ce
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-21 09:20:50 +0300

    Stored cursor and cursors manager tests.

--- src/tests/cursors-manager.ts
+++ src/tests/cursors-manager.ts
@@ -1,4 +1,9 @@
 import { assert } from 'chai';
+import * as _ from 'lodash';
+
+import {
+  Cursor,
+} from '../lib/cursor';
 
 import {
   CursorsManager,
@@ -23,6 +28,75 @@ export default function () {
         path: 'a.0.b',
         value: { d: 'e' },
       });
+    });
+    it('storaged', () => {
+      const storageCursors = {};
+      const saveCursor = (cursor) => {
+        const { id, queryId, query, data } = cursor;
+        const stored = storageCursors[id];
+        cursor.on('changed', ({ cursor }) => stored.data = cursor.data);
+        cursor.on('exec', ({ cursor: { queryId, query } }) => {
+          stored.queryId = queryId;
+          stored.query = query;
+        });
+        cursor.on('destroyed', ({ node }) => delete storageCursors[id]);
+      };
+      const loadCursor = (cursor) => {
+        const { id, queryId, query, data } = cursor;
+        let stored;
+        if (storageCursors[id]) {
+          stored = storageCursors[id];
+          cursor.queryId = stored.queryId;
+          cursor.query = stored.query;
+          cursor.data = stored.data;
+        } else {
+          stored = { queryId, query, data };
+          storageCursors[id] = stored;
+        }
+      };
+      const storageManagers = {};
+      const saveManager = (manager) => {
+        const { id, queryId, query, data } = manager;
+        manager.on('add', ({ node }) => storageManagers[manager.id] = node.id);
+        manager.on('removed', ({ node }) => delete storageManagers[manager.id]);
+      };
+      const loadManager = (manager) => {
+        const { id, nodes } = manager;
+        _.each(nodes, (node) => {
+          storageManagers[manager.id] = node.id;
+        });
+      };
+
+      class StoredCursor extends Cursor {
+        constructor(...args) {
+          super(...args);
+          loadCursor(this);
+          saveCursor(this);
+        }
+      }
+
+      class StoredCursorsManager extends CursorsManager {
+        Node = StoredCursor;
+        constructor(...args) {
+          super(...args);
+          loadManager(this);
+          saveManager(this);
+        }
+      }
+
+      let manager;
+      let cursor;
+
+      manager = new StoredCursorsManager('cursors');
+      cursor = manager.create('a');
+      cursor.apply({ type: 'set', path: '', value: { x: 123 } });
+      
+      manager = undefined;
+      cursor = undefined;
+
+      manager = new StoredCursorsManager('cursors');
+      cursor = manager.create('a');
+      assert.deepEqual(cursor.data, { x: 123 });
     });
   });
 }
--- tests/cursors-manager.js
+++ tests/cursors-manager.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const chai_1 = require("chai");
+const _ = require("lodash");
+const cursor_1 = require("../lib/cursor");
 const cursors_manager_1 = require("../lib/cursors-manager");
 function default_1() {
     describe('CursorsManager:', () => {
@@ -19,6 +21,70 @@ function default_1() {
                 path: 'a.0.b',
                 value: { d: 'e' },
             });
+        });
+        it('storaged', () => {
+            const storageCursors = {};
+            const saveCursor = (cursor) => {
+                const { id, queryId, query, data } = cursor;
+                const stored = storageCursors[id];
+                cursor.on('changed', ({ cursor }) => stored.data = cursor.data);
+                cursor.on('exec', ({ cursor: { queryId, query } }) => {
+                    stored.queryId = queryId;
+                    stored.query = query;
+                });
+                cursor.on('destroyed', ({ node }) => delete storageCursors[id]);
+            };
+            const loadCursor = (cursor) => {
+                const { id, queryId, query, data } = cursor;
+                let stored;
+                if (storageCursors[id]) {
+                    stored = storageCursors[id];
+                    cursor.queryId = stored.queryId;
+                    cursor.query = stored.query;
+                    cursor.data = stored.data;
+                }
+                else {
+                    stored = { queryId, query, data };
+                    storageCursors[id] = stored;
+                }
+            };
+            const storageManagers = {};
+            const saveManager = (manager) => {
+                const { id, queryId, query, data } = manager;
+                manager.on('add', ({ node }) => storageManagers[manager.id] = node.id);
+                manager.on('removed', ({ node }) => delete storageManagers[manager.id]);
+            };
+            const loadManager = (manager) => {
+                const { id, nodes } = manager;
+                _.each(nodes, (node) => {
+                    storageManagers[manager.id] = node.id;
+                });
+            };
+            class StoredCursor extends cursor_1.Cursor {
+                constructor(...args) {
+                    super(...args);
+                    loadCursor(this);
+                    saveCursor(this);
+                }
+            }
+            class StoredCursorsManager extends cursors_manager_1.CursorsManager {
+                constructor(...args) {
+                    super(...args);
+                    this.Node = StoredCursor;
+                    loadManager(this);
+                    saveManager(this);
+                }
+            }
+            let manager;
+            let cursor;
+            manager = new StoredCursorsManager('cursors');
+            cursor = manager.create('a');
+            cursor.apply({ type: 'set', path: '', value: { x: 123 } });
+            manager = undefined;
+            cursor = undefined;
+            manager = new StoredCursorsManager('cursors');
+            cursor = manager.create('a');
+            chai_1.assert.deepEqual(cursor.data, { x: 123 });
         });
     });
 }
